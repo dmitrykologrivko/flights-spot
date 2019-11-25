435abdc371b209219a6bd953b67b3b01c598900d
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2019-11-25 15:24:04 +0300

    Improved imports

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from './core/config.service';
+import { ConfigService } from '@core/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
